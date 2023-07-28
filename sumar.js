cf71e81b1266bb2de55fa033815afd6fb14e1747
Lucasmunizaga <140611398+Lucasmunizaga@users.noreply.github.com>
2023-07-27 22:35:46 -0400

    parche pequeño
    un pequeño parche

--- sumar.js
+++ sumar.js
@@ -1,27 +1,24 @@
-let arreglo = ["","","","",""];
-function allowDrop(ev)
-{
+let arreglo = ["", "", "", "", ""];
+
+function allowDrop(ev) {
     ev.preventDefault();
 }
-function drag(ev)
-{
-    dataTransfer.setData("text", ev.target.id);
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
 }
-function drop(ev)
-{
-    if(arreglo[parseInt(ev.target.id)] == "")
-    {
+
+function drop(ev) {
+    if (arreglo[parseInt(ev.target.id)] === "") {
         var data = ev.dataTransfer.getData("text");
         arreglo[parseInt(ev.target.id)] = data;
         ev.target.appendChild(document.getElementById(data));
     }
-    if(arreglo[0] != "" && arreglo[1] != "" && arreglo[2] != "" && arreglo[3] != "" && arreglo[4] != "")
-    {
-        if(arreglo[0] == "g" && arreglo[1] == "f" && arreglo[2] == "c" && arreglo[3] == "d" && arreglo[4] == "b")
-        {
-            document.querySelector("h2").innerHTML = "Excelente...";
-        }else{
-            document.querySelector("h2").innerHTML = "Que Mal, intentalo de nuevo...";
+    if (arreglo[0] != "" && arreglo[1] != "" && arreglo[2] != "" && arreglo[3] != "" && arreglo[4] != "") {
+        if (arreglo[0] == "g" && arreglo[1] == "f" && arreglo[2] == "c" && arreglo[3] == "d" && arreglo[4] == "b") {
+            document.querySelector("h2").innerHTML = "¡Excelente!";
+        } else {
+            document.querySelector("h2").innerHTML = "¡Qué mal, inténtalo de nuevo!";
         }
     }
 }
